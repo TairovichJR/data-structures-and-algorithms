cd8e212d7c72d2f6ccf12f67e93f01c8b169c167
TairovichJR <yokubov.omonjon@gmail.com>
2020-12-08 23:40:34 -0500

    FrogJump solution 2

--- src/codility/FrogJump.java
+++ src/codility/FrogJump.java
@@ -4,18 +4,28 @@ public class FrogJump {
 
     public static void main(String[] args) {
 
-        System.out.println(solution(3, 999111321, 7));
+        System.out.println(solution2(3, 999111321, 7));
     }
 
     public static int solution(int X, int Y, int D) {
 
         int distance = (Y-X);
-
         if (distance % D == 0 ){
             return distance/D;
         }else{
             return distance/D + 1;
         }
+    }
+
+    public static int solution2(int X, int Y, int D) {
+
+        int jump = X;
+        int count = 0;
+        while (jump < Y){
+            jump += D;
+            count++;
+        }
+        return count;
     }
 
 }
